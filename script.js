71b44f6b4a484cdd76bda4aa2e7ca385128b93a0
a18jakme <49142935+a18jakme@users.noreply.github.com>
2021-03-22 16:03:23 +0100

    Same random icon
    - Seedrandom that generate same random icon places. same variable used for both skeumorph and flat matrix.
    
    - Change some missspellings.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var myrng = new Math.seedrandom('randomicon');
 var SkeuomorphIconArray = 
 ["<img onclick='messageFunction(this.id)' id=Hamburger-icon class=iconimage src=Skeumorph-icons/hamburger-icon/57.png>",
 "<img onclick='messageFunction(this.id)' id=Computer-icon class=iconimage src=Skeumorph-icons/computer-icon/57.png>",
@@ -9,7 +10,7 @@ var SkeuomorphIconArray =
 "<img onclick='messageFunction(this.id)' id=Camera-icon class=iconimage src=Skeumorph-icons/camera-icon/57.png>",
 "<img onclick='messageFunction(this.id)' id=Trashcan-icon class=iconimage src=Skeumorph-icons/trashcan-icon/57.png>",
 "<img onclick='messageFunction(this.id)' id=Headphones-icon class=iconimage src=Skeumorph-icons/headphones-icon/57.png>",
-"<img onclick='messageFunction(this.id)' id=Pensil-icon class=iconimage src=Skeumorph-icons/pensil-icon/57.png>",
+"<img onclick='messageFunction(this.id)' id=Pencil-icon class=iconimage src=Skeumorph-icons/pencil-icon/57.png>",
 "<img onclick='messageFunction(this.id)' id=Search-icon class=iconimage src=Skeumorph-icons/search-icon/57.png>"];
 var FlatIconArray = 
 ["<img onclick='messageFunction(this.id)' id=Hamburger-icon class=iconimage src=Flatdesign-icons/hamburger-icon/57.png>",
@@ -22,10 +23,10 @@ var FlatIconArray =
 "<img onclick='messageFunction(this.id)' id=Camera-icon class=iconimage src=Flatdesign-icons/camera-icon/57.png>",
 "<img onclick='messageFunction(this.id)' id=Trashcan-icon class=iconimage src=Flatdesign-icons/trashcan-icon/57.png>",
 "<img onclick='messageFunction(this.id)' id=Headphones-icon class=iconimage src=Flatdesign-icons/headphones-icon/57.png>",
-"<img onclick='messageFunction(this.id)' id=Pensil-icon class=iconimage src=Flatdesign-icons/pensil-icon/57.png>",
+"<img onclick='messageFunction(this.id)' id=Pencil-icon class=iconimage src=Flatdesign-icons/pencil-icon/57.png>",
 "<img onclick='messageFunction(this.id)' id=Search-icon class=iconimage src=Flatdesign-icons/search-icon/57.png>"];
 var textQuote = ["Hamburger-icon", "Computer-icon", "Donut-icon", "Coffee-icon", "Book-icon", "Glasses-icon", "Speaker-icon", "Camera-icon", "Trashcan-icon", "Headphones-icon", 
-"Pensil-icon","Search-icon"];
+"Pencil-icon","Search-icon"];
 var rand;
 var skeumorphMatrix;
 var flatdesignMatrix;
@@ -33,77 +34,64 @@ var flatdesignMatrix;
 function onloadFunction(){
     skeumorphMatrix = document.getElementById("skeumorphmatrix");
     flatdesignMatrix = document.getElementById("flatdesignmatrix");
+    skeumorphMatrix.style.display='block';
     randomQuote();
+    randomIcon();
 }
 function changeStyle(){
-    if(skeumorphMatrix.display === 'block'){
-        skeumorphMatrix.display='none';
-        flatdesignMatrix.display='block';
-        console.log("flat");
-        randomFlatdesignIcon();
+    if(skeumorphMatrix.style.display == 'block'){
+        skeumorphMatrix.style.display='none';
+        flatdesignMatrix.style.display='block';
     }
     else{
-        skeumorphMatrix.display='block';
-        flatdesignMatrix.display='none';
-        console.log("skeu");
-        randomSkeuomoprhismIcon();
+        skeumorphMatrix.style.display='block';
+        flatdesignMatrix.style.display='none';
     }
 }
 function randomQuote(){
-    rand = Math.floor(Math.random() * textQuote.length);
+    rand = Math.floor(myrng() * textQuote.length);
     document.getElementById('textinstruction').innerHTML = textQuote[rand];
 }
-function randomSkeuomoprhismIcon(){
+function randomIcon(){
     document.getElementById('message').innerHTML = "";
     for (var i = SkeuomorphIconArray.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
+        var j = Math.floor(myrng() * (i + 1));
         var tempArray = SkeuomorphIconArray[i];
         SkeuomorphIconArray[i] = SkeuomorphIconArray[j];
         SkeuomorphIconArray[j] = tempArray;
-        document.getElementById("icon1").innerHTML = SkeuomorphIconArray[0];
-        document.getElementById("icon2").innerHTML = SkeuomorphIconArray[1];
-        document.getElementById("icon3").innerHTML = SkeuomorphIconArray[2];
-        document.getElementById("icon4").innerHTML = SkeuomorphIconArray[3];
-        document.getElementById("icon5").innerHTML = SkeuomorphIconArray[4];
-        document.getElementById("icon6").innerHTML = SkeuomorphIconArray[5];
-        document.getElementById("icon7").innerHTML = SkeuomorphIconArray[6];
-        document.getElementById("icon8").innerHTML = SkeuomorphIconArray[7];
-        document.getElementById("icon9").innerHTML = SkeuomorphIconArray[8];
-        document.getElementById("icon10").innerHTML = SkeuomorphIconArray[9];
-        document.getElementById("icon11").innerHTML = SkeuomorphIconArray[10];
-        document.getElementById("icon12").innerHTML = SkeuomorphIconArray[11];
-    }
-}
-function randomFlatdesignIcon(){
-    document.getElementById('message').innerHTML = "";
-    for (var i = FlatIconArray.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var tempArray = FlatIconArray[i];
+        tempArray=FlatIconArray[i];
         FlatIconArray[i] = FlatIconArray[j];
         FlatIconArray[j] = tempArray;
-        document.getElementById("icon1").innerHTML = FlatIconArray[0];
-        document.getElementById("icon2").innerHTML = FlatIconArray[1];
-        document.getElementById("icon3").innerHTML = FlatIconArray[2];
-        document.getElementById("icon4").innerHTML = FlatIconArray[3];
-        document.getElementById("icon5").innerHTML = FlatIconArray[4];
-        document.getElementById("icon6").innerHTML = FlatIconArray[5];
-        document.getElementById("icon7").innerHTML = FlatIconArray[6];
-        document.getElementById("icon8").innerHTML = FlatIconArray[7];
-        document.getElementById("icon9").innerHTML = FlatIconArray[8];
-        document.getElementById("icon10").innerHTML = FlatIconArray[9];
-        document.getElementById("icon11").innerHTML = FlatIconArray[10];
-        document.getElementById("icon12").innerHTML = FlatIconArray[11];
+        document.getElementById("sicon1").innerHTML = SkeuomorphIconArray[0];
+        document.getElementById("sicon2").innerHTML = SkeuomorphIconArray[1];
+        document.getElementById("sicon3").innerHTML = SkeuomorphIconArray[2];
+        document.getElementById("sicon4").innerHTML = SkeuomorphIconArray[3];
+        document.getElementById("sicon5").innerHTML = SkeuomorphIconArray[4];
+        document.getElementById("sicon6").innerHTML = SkeuomorphIconArray[5];
+        document.getElementById("sicon7").innerHTML = SkeuomorphIconArray[6];
+        document.getElementById("sicon8").innerHTML = SkeuomorphIconArray[7];
+        document.getElementById("sicon9").innerHTML = SkeuomorphIconArray[8];
+        document.getElementById("sicon10").innerHTML = SkeuomorphIconArray[9];
+        document.getElementById("sicon11").innerHTML = SkeuomorphIconArray[10];
+        document.getElementById("sicon12").innerHTML = SkeuomorphIconArray[11];
+        document.getElementById("ficon1").innerHTML = FlatIconArray[0];
+        document.getElementById("ficon2").innerHTML = FlatIconArray[1];
+        document.getElementById("ficon3").innerHTML = FlatIconArray[2];
+        document.getElementById("ficon4").innerHTML = FlatIconArray[3];
+        document.getElementById("ficon5").innerHTML = FlatIconArray[4];
+        document.getElementById("ficon6").innerHTML = FlatIconArray[5];
+        document.getElementById("ficon7").innerHTML = FlatIconArray[6];
+        document.getElementById("ficon8").innerHTML = FlatIconArray[7];
+        document.getElementById("ficon9").innerHTML = FlatIconArray[8];
+        document.getElementById("ficon10").innerHTML = FlatIconArray[9];
+        document.getElementById("ficon11").innerHTML = FlatIconArray[10];
+        document.getElementById("ficon12").innerHTML = FlatIconArray[11];
     }
 }
 function messageFunction(iconName){
     if(iconName == textQuote[rand]){
         randomQuote();
-        if(skeumorphMatrix.display === 'block'){
-            randomSkeuomoprhismIcon();
-        }
-        else{
-            randomFlatdesignIcon();
-        }
+        randomIcon();
     }
     else{
         document.getElementById('message').innerHTML = "Wrong";
