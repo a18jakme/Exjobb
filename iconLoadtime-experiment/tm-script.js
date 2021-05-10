7bdbf8a81b433a8144c1c764265e7998a91a0a5d
a18jakme <49142935+a18jakme@users.noreply.github.com>
2021-05-10 13:56:42 +0200

    change script
    add pictures

--- iconLoadtime-experiment/tm-script.js
+++ iconLoadtime-experiment/tm-script.js
@@ -2,9 +2,9 @@
 // @name         iconLoadtime
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  script to download the page load time
+// @description  try to take over the world!
 // @author       You
-// @match        file:///C:/Users/jakob/OneDrive/Documents/GitHub/Exjobb/iconLoadtime-experiment/index.html
+// @match        file:///C:/Users/jakob/OneDrive/Documents/GitHub/Exjobb/iconLoadtime-experiment/skeuomorphism-iconLoadtime.html
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
 // @icon         https://www.google.com/s2/favicons?domain=undefined.
 // @grant        none
@@ -12,26 +12,34 @@
 
 (function() {
     'use strict';
-    var pageLoad = 5;
+    var measurement = new Date();
+    var pageLoad = 20;
     var str = localStorage.getItem("theData");
     var cnt = parseInt(localStorage.getItem("counter"));
-    // calculates the time it takes for the web page to load
-    var delta = performance.timing.domComplete - performance.timing.connectStart;
-
-    if(isNaN(cnt)) cnt=0;
-    if (cnt == 0) {
-        str="data:text/csv;charset=utf-8";
-    }
-    if(cnt == pageLoad){
-        getData();
+    if(cnt >= pageLoad){
+        //getData();
         localStorage.clear();
     }
     else{
-        // Increase counter and save data to localstorage
-        str += " , " + delta;
+        if(isNaN(cnt)) cnt=0;
+        if (cnt == 0) {
+            str="data:text/csv;charset=utf-8";
+        }
+        else{
+            var old = new Date();
+            old = old.setTime(localStorage.getItem("Oldval"));
+            var delta = measurement-old;
+            // Increase counter and save data to localstorage
+            str += " , " + delta;
+            console.log(delta);
+        }
+        console.log(cnt);
         cnt ++;
         localStorage.setItem("theData", str);
         localStorage.setItem("counter", cnt);
+        var measurement2 = new Date();
+        measurement2 = measurement2.getTime();
+        localStorage.setItem("Oldval",measurement2);
         location.reload();
     }
     function getData() {
